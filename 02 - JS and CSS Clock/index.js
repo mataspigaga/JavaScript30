25a687f690f29e307c42a714747338d74093c474
Matas Pigaga <mataspigaga@gmail.com>
2021-09-04 14:47:54 -0400

    slight update

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -7,16 +7,12 @@ function setDate() {
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90; // offset the default 90 degree transformation
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
   const minutes = now.getMinutes();
   const minutesDegrees = (minutes / 60) * 360 + 90; // offset the default 90 degree transformation
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
   const hours = now.getHours();
   const hoursDegrees = (hours / 12) * 360 + 90; // offset the default 90 degree transformation
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
   console.log(seconds, minutes, hours);
 }
-
 setInterval(setDate, 1000);
